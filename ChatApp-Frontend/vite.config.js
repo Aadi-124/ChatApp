ecf970bb788b7f10fa9eea95c02fded34b825fab
Aaditya Thakare <aadityathakare124@gmail.com>
2025-04-10 14:44:52 +0530

    Sample

--- ChatApp-Frontend/vite.config.js
+++ ChatApp-Frontend/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    global: 'window', // Ensure `global` is defined as `window`
+  },
 })
