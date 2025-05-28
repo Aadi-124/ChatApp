203110e9dfe96c1c95c6f3f7af05e51821c36529
Aaditya Thakare <aadityathakare124@gmail.com>
2025-05-28 10:00:18 +0530

    Allowed OriginPAttern from Deployed Frontend!

--- ChatApp-Backend/src/main/java/com/chatapp/ChatApp/Configuration/WebSocketConfiguration.java
+++ ChatApp-Backend/src/main/java/com/chatapp/ChatApp/Configuration/WebSocketConfiguration.java
@@ -20,7 +20,7 @@ public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer 
 
     public void registerStompEndpoints(StompEndpointRegistry registry){
         registry.addEndpoint("/chat")
-                .setAllowedOriginPatterns("http://localhost:5173")
+                .setAllowedOriginPatterns("https://chatapp-sbxq.onrender.com")
                 .withSockJS();
     }
 
